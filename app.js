595eafb65b87370e653daac067b2df0fdc47e25d
dev-vicki <vignesh.r1999@gmail.com>
2022-01-11 17:39:00 +0530

    imageCarousel using Api

---
+++ app.js
@@ -0,0 +1,15 @@
+const image = document.getElementById("center");
+
+fetch("https://jsonplaceholder.typicode.com/photos?_limit=20")
+.then((res) => {
+  return res.json();
+})
+.then((data) =>{
+    data.map((value) => {
+        key=value.id;
+        let img = document.createElement("img");
+        img.src = `https://picsum.photos/600/600?random=${value.id}`;
+        center.appendChild(img);
+        console.log(value.url)
+    })
+})
